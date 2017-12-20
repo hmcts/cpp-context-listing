8ba01cdb8f3f9334572e6f94a41dce64a5dd1526
paddycakes <paddy.gallagher@gmail.com>
2017-12-20 11:52:15 +0000

    GPE-2418: Improve Listing context code quality
    Change-Id: Ifce3a43f27ec2d8424f5e8e6b8173da99ef59efd

--- listing-integration-test/src/test/java/uk/gov/moj/cpp/listing/utils/ReferenceDataStub.java
+++ listing-integration-test/src/test/java/uk/gov/moj/cpp/listing/utils/ReferenceDataStub.java
@@ -32,32 +32,11 @@ public class ReferenceDataStub {
     private static final String FIELD_FIRST_NAME = "firstName";
     private static final String FIELD_LAST_NAME = "lastName";
 
-
-    private static final String REFERENCE_DATA_RESULT_DEFINITIONS_QUERY_URL = "/referencedata-query-api/query/api/rest/referencedata/result/definitions";
-    private static final String REFERENCE_DATA_RESULT_DEFINITIONS_KEYWORDS_QUERY_URL = "/referencedata-query-api/query/api/rest/referencedata/result/definition-keyword-synonyms";
-    private static final String REFERENCE_DATA_RESULT_PROMPTS_QUERY_URL = "/referencedata-query-api/query/api/rest/referencedata/result/prompts";
     private static final String REFERENCE_DATA_COURT_CENTRE_QUERY_URL = "/referencedata-query-api/query/api/rest/referencedata/court/centres/{0}";
     private static final String REFERENCE_DATA_JUDGE_QUERY_URL = "/referencedata-query-api/query/api/rest/referencedata/court/judges/{0}";
-    private static final String REFERENCE_DATA_RESULT_PROMPT_FIXED_LISTS_QUERY_URL = "/referencedata-query-api/query/api/rest/referencedata/result/prompt-fixedlists";
-    private static final String REFERENCE_DATA_RESULT_PROMPTS_KEYWORDS_QUERY_URL = "/referencedata-query-api/query/api/rest/referencedata/result/prompt-keyword-synonyms";
 
-
-    private static final String REFERENCE_DATA_RESULT_DEFINITIONS_MEDIA_TYPE = "application/vnd.referencedata.result.get-all-definitions+json";
-    private static final String REFERENCE_DATA_RESULT_DEFINITIONS_KEYWORDS_MEDIA_TYPE = "application/vnd.referencedata.result.get-all-definition-keyword-synonyms";
-    private static final String REFERENCE_DATA_RESULT_PROMPTS_MEDIA_TYPE = "application/vnd.referencedata.result.get-all-prompts+json";
     private static final String REFERENCE_DATA_JUDGE_MEDIA_TYPE = "application/vnd.referencedata.get-judge+json";
     private static final String REFERENCE_DATA_COURT_CENTRE_MEDIA_TYPE = "application/vnd.referencedata.get.court-centre+json";
-    private static final String REFERENCE_DATA_RESULT_PROMPT_FIXED_LISTS_MEDIA_TYPE = "application/vnd.referencedata.result.get-all-prompt-fixedlists+json";
-    private static final String REFERENCE_DATA_RESULT_PROMPTS_KEYWORDS_MEDIA_TYPE = "application/vnd.referencedata.result.get-all-prompt-keyword-synonyms+json";
-
-
-    public static void stubForReferenceDataResults(){
-        stubGetReferenceDataResultDefinitions();
-        stubGetReferenceDataResultDefinitionsKeywords();
-        stubGetReferenceDataResultPrompts();
-        stubGetReferenceDataResultPromptsKeywords();
-        stubGetReferenceDataResultPromptFixedLists();
-    }
 
     public static void stubGetReferenceDataCourtCentre(final CourtReferenceData courtReferenceData) {
         InternalEndpointMockUtils.stubPingFor("referencedata-query-api");
@@ -84,69 +63,6 @@ public class ReferenceDataStub {
         waitForStubToBeReady(format(REFERENCE_DATA_JUDGE_QUERY_URL, judge.getJudgeId()), REFERENCE_DATA_JUDGE_MEDIA_TYPE);
     }
 
-
-
-    private static void stubGetReferenceDataResultDefinitions() {
-        InternalEndpointMockUtils.stubPingFor("referencedata-query-api");
-
-        stubFor(get(urlPathEqualTo(REFERENCE_DATA_RESULT_DEFINITIONS_QUERY_URL))
-                .willReturn(aResponse().withStatus(SC_OK)
-                        .withHeader("CPPID", UUID.randomUUID().toString())
-                        .withHeader("Content-Type", REFERENCE_DATA_RESULT_DEFINITIONS_MEDIA_TYPE)
-                        .withBody(getPayload("referencedata.result.definitions.json"))));
-
-        waitForStubToBeReady(REFERENCE_DATA_RESULT_DEFINITIONS_QUERY_URL, REFERENCE_DATA_RESULT_DEFINITIONS_MEDIA_TYPE);
-    }
-
-    private static void stubGetReferenceDataResultDefinitionsKeywords() {
-        InternalEndpointMockUtils.stubPingFor("referencedata-query-api");
-
-        stubFor(get(urlPathEqualTo(REFERENCE_DATA_RESULT_DEFINITIONS_KEYWORDS_QUERY_URL))
-                .willReturn(aResponse().withStatus(SC_OK)
-                        .withHeader("CPPID", UUID.randomUUID().toString())
-                        .withHeader("Content-Type", REFERENCE_DATA_RESULT_DEFINITIONS_KEYWORDS_MEDIA_TYPE)
-                        .withBody(getPayload("referencedata.result.definition-keyword-synonyms.json"))));
-
-        waitForStubToBeReady(REFERENCE_DATA_RESULT_DEFINITIONS_KEYWORDS_QUERY_URL, REFERENCE_DATA_RESULT_DEFINITIONS_KEYWORDS_MEDIA_TYPE);
-    }
-
-    private static void stubGetReferenceDataResultPrompts() {
-        InternalEndpointMockUtils.stubPingFor("referencedata-query-api");
-
-        stubFor(get(urlPathEqualTo(REFERENCE_DATA_RESULT_PROMPTS_QUERY_URL))
-                .willReturn(aResponse().withStatus(SC_OK)
-                        .withHeader("CPPID", UUID.randomUUID().toString())
-                        .withHeader("Content-Type", REFERENCE_DATA_RESULT_PROMPTS_MEDIA_TYPE)
-                        .withBody(getPayload("referencedata.result.prompts.json"))));
-
-        waitForStubToBeReady(REFERENCE_DATA_RESULT_PROMPTS_QUERY_URL, REFERENCE_DATA_RESULT_PROMPTS_MEDIA_TYPE);
-    }
-
-    private static void stubGetReferenceDataResultPromptFixedLists() {
-        InternalEndpointMockUtils.stubPingFor("referencedata-query-api");
-
-        stubFor(get(urlPathEqualTo(REFERENCE_DATA_RESULT_PROMPT_FIXED_LISTS_QUERY_URL))
-                .willReturn(aResponse().withStatus(SC_OK)
-                        .withHeader("CPPID", UUID.randomUUID().toString())
-                        .withHeader("Content-Type", REFERENCE_DATA_RESULT_PROMPT_FIXED_LISTS_MEDIA_TYPE)
-                        .withBody(getPayload("referencedata.result.prompt-fixedlists.json"))));
-
-        waitForStubToBeReady(REFERENCE_DATA_RESULT_PROMPT_FIXED_LISTS_QUERY_URL, REFERENCE_DATA_RESULT_PROMPT_FIXED_LISTS_MEDIA_TYPE);
-    }
-
-    private static void stubGetReferenceDataResultPromptsKeywords() {
-        InternalEndpointMockUtils.stubPingFor("referencedata-query-api");
-
-        stubFor(get(urlPathEqualTo(REFERENCE_DATA_RESULT_PROMPTS_KEYWORDS_QUERY_URL ))
-                .willReturn(aResponse().withStatus(SC_OK)
-                        .withHeader("CPPID", UUID.randomUUID().toString())
-                        .withHeader("Content-Type", REFERENCE_DATA_RESULT_PROMPTS_KEYWORDS_MEDIA_TYPE)
-                        .withBody(getPayload("referencedata.result.prompt-keyword-synonyms.json"))));
-
-        waitForStubToBeReady(REFERENCE_DATA_RESULT_PROMPTS_KEYWORDS_QUERY_URL, REFERENCE_DATA_RESULT_PROMPTS_KEYWORDS_MEDIA_TYPE);
-    }
-
-
     private static JsonObjectBuilder getCourtCentreJsonBuilder(final CourtReferenceData rd) {
         final JsonArrayBuilder courtRooms = createArrayBuilder()
                 .add(createObjectBuilder()
@@ -162,17 +78,12 @@ public class ReferenceDataStub {
                 .add(FIELD_COURT_ROOMS, courtRooms.build());
     }
 
-
-
     private static JsonObjectBuilder getJudgeJsonBuilder(final Judge judge) {
         return createObjectBuilder()
                 .add(FIELD_GENERIC_ID, judge.getJudgeId().toString())
                 .add(FIELD_TITLE, judge.getJudgeTitle())
                 .add(FIELD_FIRST_NAME, judge.getJudgeFirstName())
                 .add(FIELD_LAST_NAME, judge.getJudgeLastName());
-
     }
-
-
 
 }
