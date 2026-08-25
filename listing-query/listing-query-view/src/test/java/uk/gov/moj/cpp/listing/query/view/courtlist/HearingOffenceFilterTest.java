0c01fb6073136328a4775791739eb65644ed62e6
Ertugrul Duran <ertugrul.duran@hmcts.net>
2026-08-25 13:52:07 +0100

    Fixing DLQ on public.listing.court-list-published

--- listing-query/listing-query-view/src/test/java/uk/gov/moj/cpp/listing/query/view/courtlist/HearingOffenceFilterTest.java
+++ listing-query/listing-query-view/src/test/java/uk/gov/moj/cpp/listing/query/view/courtlist/HearingOffenceFilterTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
@@ -54,6 +56,206 @@ public class HearingOffenceFilterTest {
         assertThat(remainingHearings.size(), is(2));
         assertThat(remainingHearings.getJsonObject(0).getString("startTime"), is("has-offence"));
         assertThat(remainingHearings.getJsonObject(1).getString("startTime"), is("no-defendants"));
+    }
+
+    @Test
+    public void shouldRemoveHearingWhenDefendantOffencesIsJsonNull() {
+
+        final JsonObject defendantWithNullOffences = createObjectBuilder().addNull("offences").build();
+        final JsonObject hearing = hearingWithDefendant("null-offences", defendantWithNullOffences);
+
+        final JsonObject payload = courtListPayloadWithHearings(hearing);
+
+        final JsonObject result = HearingOffenceFilter.removeHearingsWithDefendantMissingOffences(payload);
+
+        assertThat(firstSitting(result).getJsonArray("hearings").size(), is(0));
+    }
+
+    @Test
+    public void shouldKeepHearingWhenDefendantsArrayIsEmpty() {
+
+        final JsonObject hearing = createObjectBuilder()
+                .add("startTime", "no-defendants-listed")
+                .add("defendants", createArrayBuilder().build())
+                .build();
+
+        final JsonObject payload = courtListPayloadWithHearings(hearing);
+
+        final JsonObject result = HearingOffenceFilter.removeHearingsWithDefendantMissingOffences(payload);
+
+        final JsonArray remainingHearings = firstSitting(result).getJsonArray("hearings");
+        assertThat(remainingHearings.size(), is(1));
+        assertThat(remainingHearings.getJsonObject(0).getString("startTime"), is("no-defendants-listed"));
+    }
+
+    @Test
+    public void shouldReturnPayloadUnchangedWhenCourtListsKeyIsMissing() {
+
+        final JsonObject payload = createObjectBuilder().add("courtCentreId", "abc-123").build();
+
+        assertThat(HearingOffenceFilter.removeHearingsWithDefendantMissingOffences(payload), is(payload));
+    }
+
+    @Test
+    public void shouldReturnPayloadUnchangedWhenCourtListsIsJsonNull() {
+
+        final JsonObject payload = createObjectBuilder().add("courtCentreId", "abc-123").addNull("courtLists").build();
+
+        assertThat(HearingOffenceFilter.removeHearingsWithDefendantMissingOffences(payload), is(payload));
+    }
+
+    @Test
+    public void shouldKeepCourtListUnchangedWhenSittingsKeyIsMissing() {
+
+        final JsonObject crestCourtSite = createObjectBuilder().add("crestCourtSiteId", "415").build();
+        final JsonObject courtListWithoutSittings = createObjectBuilder().add("crestCourtSite", crestCourtSite).build();
+        final JsonObject payload = createObjectBuilder()
+                .add("courtLists", createArrayBuilder().add(courtListWithoutSittings).build())
+                .build();
+
+        assertThat(HearingOffenceFilter.removeHearingsWithDefendantMissingOffences(payload), is(payload));
+    }
+
+    @Test
+    public void shouldKeepCourtListUnchangedWhenSittingsIsJsonNull() {
+
+        final JsonObject courtListWithNullSittings = createObjectBuilder().addNull("sittings").build();
+        final JsonObject payload = createObjectBuilder()
+                .add("courtLists", createArrayBuilder().add(courtListWithNullSittings).build())
+                .build();
+
+        assertThat(HearingOffenceFilter.removeHearingsWithDefendantMissingOffences(payload), is(payload));
+    }
+
+    @Test
+    public void shouldKeepSittingUnchangedWhenHearingsKeyIsMissing() {
+
+        final JsonObject sittingWithoutHearings = createObjectBuilder()
+                .add("sittingDate", "2026-01-01")
+                .add("judiciary", createArrayBuilder().add(createObjectBuilder().add("judicialId", "j-1").build()).build())
+                .build();
+
+        final JsonObject payload = courtListPayloadWithSittings(sittingWithoutHearings);
+
+        assertThat(HearingOffenceFilter.removeHearingsWithDefendantMissingOffences(payload), is(payload));
+    }
+
+    @Test
+    public void shouldKeepSittingUnchangedWhenHearingsIsJsonNull() {
+
+        final JsonObject sittingWithNullHearings = createObjectBuilder().addNull("hearings").build();
+
+        final JsonObject payload = courtListPayloadWithSittings(sittingWithNullHearings);
+
+        assertThat(HearingOffenceFilter.removeHearingsWithDefendantMissingOffences(payload), is(payload));
+    }
+
+    @Test
+    public void shouldPreserveSiblingFieldsAtEveryLevelWhenNothingIsRemoved() {
+
+        final JsonObject defendantWithOffence = createObjectBuilder()
+                .add("offences", createArrayBuilder().add(OFFENCE).build())
+                .build();
+        final JsonObject survivingHearing = createObjectBuilder()
+                .add("startTime", "2026-01-01T10:00:00")
+                .add("hasVideoLink", true)
+                .add("defendants", createArrayBuilder().add(defendantWithOffence).build())
+                .build();
+        final JsonObject crestCourtSite = createObjectBuilder().add("crestCourtSiteId", "415").build();
+        final JsonObject sitting = createObjectBuilder()
+                .add("sittingDate", "2026-01-01")
+                .add("judiciary", createArrayBuilder().add(createObjectBuilder().add("judicialId", "j-1").build()).build())
+                .add("hearings", createArrayBuilder().add(survivingHearing).build())
+                .build();
+        final JsonObject courtList = createObjectBuilder()
+                .add("crestCourtSite", crestCourtSite)
+                .add("sittings", createArrayBuilder().add(sitting).build())
+                .build();
+        final JsonObject payload = createObjectBuilder()
+                .add("courtCentreId", "abc-123")
+                .add("courtLists", createArrayBuilder().add(courtList).build())
+                .build();
+
+        assertThat(HearingOffenceFilter.removeHearingsWithDefendantMissingOffences(payload), is(payload));
+    }
+
+    @Test
+    public void shouldFilterIndependentlyAcrossMultipleSittingsAndCourtLists() {
+
+        final JsonObject defendantWithOffence = createObjectBuilder()
+                .add("offences", createArrayBuilder().add(OFFENCE).build())
+                .build();
+        final JsonObject defendantWithoutOffence = createObjectBuilder()
+                .add("offences", createArrayBuilder().build())
+                .build();
+
+        final JsonObject sittingOne = createObjectBuilder()
+                .add("hearings", createArrayBuilder()
+                        .add(hearingWithDefendant("keep-A", defendantWithOffence))
+                        .add(hearingWithDefendant("drop-A", defendantWithoutOffence))
+                        .build())
+                .build();
+        final JsonObject sittingTwo = createObjectBuilder()
+                .add("hearings", createArrayBuilder()
+                        .add(hearingWithDefendant("drop-B", defendantWithoutOffence))
+                        .add(hearingWithDefendant("keep-B", defendantWithOffence))
+                        .build())
+                .build();
+        final JsonObject courtListOne = createObjectBuilder()
+                .add("sittings", createArrayBuilder().add(sittingOne).add(sittingTwo).build())
+                .build();
+
+        final JsonObject sittingThree = createObjectBuilder()
+                .add("hearings", createArrayBuilder().add(hearingWithDefendant("keep-C", defendantWithOffence)).build())
+                .build();
+        final JsonObject courtListTwo = createObjectBuilder()
+                .add("sittings", createArrayBuilder().add(sittingThree).build())
+                .build();
+
+        final JsonObject payload = createObjectBuilder()
+                .add("courtLists", createArrayBuilder().add(courtListOne).add(courtListTwo).build())
+                .build();
+
+        final JsonObject result = HearingOffenceFilter.removeHearingsWithDefendantMissingOffences(payload);
+
+        final JsonArray courtLists = result.getJsonArray("courtLists");
+        final JsonArray sittingsOfCourtListOne = courtLists.getJsonObject(0).getJsonArray("sittings");
+        assertThat(startTimesOf(sittingsOfCourtListOne.getJsonObject(0).getJsonArray("hearings")), is(List.of("keep-A")));
+        assertThat(startTimesOf(sittingsOfCourtListOne.getJsonObject(1).getJsonArray("hearings")), is(List.of("keep-B")));
+
+        final JsonArray sittingsOfCourtListTwo = courtLists.getJsonObject(1).getJsonArray("sittings");
+        assertThat(startTimesOf(sittingsOfCourtListTwo.getJsonObject(0).getJsonArray("hearings")), is(List.of("keep-C")));
+    }
+
+    @Test
+    public void privateConstructorShouldThrowIllegalStateException() {
+        assertThrows(InvocationTargetException.class, () -> {
+            final Constructor<HearingOffenceFilter> constructor = HearingOffenceFilter.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            constructor.newInstance();
+        });
+    }
+
+    private static JsonObject hearingWithDefendant(final String startTime, final JsonObject defendant) {
+        return createObjectBuilder()
+                .add("startTime", startTime)
+                .add("defendants", createArrayBuilder().add(defendant).build())
+                .build();
+    }
+
+    private static List<String> startTimesOf(final JsonArray hearings) {
+        return hearings.getValuesAs(JsonObject.class).stream()
+                .map(hearing -> hearing.getString("startTime"))
+                .collect(Collectors.toList());
+    }
+
+    private static JsonObject courtListPayloadWithSittings(final JsonObject... sittings) {
+        final JsonArrayBuilder sittingsBuilder = createArrayBuilder();
+        for (final JsonObject sitting : sittings) {
+            sittingsBuilder.add(sitting);
+        }
+        final JsonObject courtList = createObjectBuilder().add("sittings", sittingsBuilder.build()).build();
+        return createObjectBuilder().add("courtLists", createArrayBuilder().add(courtList).build()).build();
     }
 
     private static JsonObject courtListPayloadWithHearings(final JsonObject... hearings) {
