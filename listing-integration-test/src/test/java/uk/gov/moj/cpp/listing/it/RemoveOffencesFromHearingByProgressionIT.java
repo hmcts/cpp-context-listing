593339c2207abf1d23e7ece82e1af91f689acf5c
koray kaya <koray.kaya3@hmcts.net>
2026-09-23 11:51:37 +0100

    SPRDT-1364 assert the allocated range-search still returns the hearing and match the published schema

--- listing-integration-test/src/test/java/uk/gov/moj/cpp/listing/it/RemoveOffencesFromHearingByProgressionIT.java
+++ listing-integration-test/src/test/java/uk/gov/moj/cpp/listing/it/RemoveOffencesFromHearingByProgressionIT.java
@@ -1,6 +1,15 @@
 package uk.gov.moj.cpp.listing.it;
 
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.withJsonPath;
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static uk.gov.justice.services.test.utils.core.http.BaseUriProvider.getBaseUri;
+import static uk.gov.justice.services.test.utils.core.http.RequestParamsBuilder.requestParams;
+import static uk.gov.justice.services.test.utils.core.matchers.ResponsePayloadMatcher.payload;
+import static uk.gov.justice.services.test.utils.core.matchers.ResponseStatusMatcher.status;
+import static uk.gov.moj.cpp.listing.it.util.RestPollerHelper.pollWithDefaults;
+import static uk.gov.moj.cpp.listing.utils.CourtSchedulerServiceStub.stubGetHearingIdsWithBody;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
@@ -14,10 +23,12 @@ import static uk.gov.moj.cpp.listing.utils.CourtSchedulerServiceStub.stubGetProv
 import static uk.gov.moj.cpp.listing.utils.CourtSchedulerServiceStub.stubListHearingInCourtSessions;
 import static uk.gov.moj.cpp.listing.utils.CourtSchedulerServiceStub.courtSchedulerCallCountForHearing;
 
+import uk.gov.justice.services.test.utils.core.http.RequestParams;
 import uk.gov.moj.cpp.listing.steps.ListCourtHearingSteps;
 import uk.gov.moj.cpp.listing.steps.RemoveOffencesFromHearingSteps;
 import uk.gov.moj.cpp.listing.steps.data.HearingsData;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -73,6 +84,29 @@ class RemoveOffencesFromHearingByProgressionIT extends AbstractIT {
         // listing request asked for, so assert it is still set rather than which room it is.
         pollForHearingByIdWithJmsDelay(USER_ID_VALUE, UUID.fromString(hearingId),
                 withJsonPath("$.hearingDays[0].courtRoomId", notNullValue()));
+
+        // AC-4 second clause: the allocated range-search (businessType + allocated + ouCode) must
+        // still return this hearing after the removal. Court-scheduler supplies only the hearing
+        // IDs; listing then enriches each one from its own viewstore, so a hearing that had been
+        // dropped from the allocated view would come back missing here even though the stub still
+        // offered its id.
+        final LocalDate hearingDate = hearings.getHearingData().get(0).getHearingStartTime().toLocalDate();
+        stubGetHearingIdsWithBody("{ \"results\": 1, \"pageCount\": 1, \"hearingIds\": ["
+                + "{\"hearingId\":\"" + hearingId + "\",\"courtScheduleId\":\"" + COURT_SCHEDULE_ID + "\","
+                + "\"hearingDate\":\"" + hearingDate + "\",\"hearingDayCount\":1,\"hearingDayPosition\":1}"
+                + "] }");
+
+        final String rangeSearchUrl = String.format("%s/listing-query-api/query/api/rest/listing/hearings/range-search"
+                        + "?allocated=true&jurisdictionType=MAGISTRATES&businessType=REM&ouCode=B04KO00"
+                        + "&sessionStartDate=%s&sessionEndDate=%s&pageSize=10&pageNumber=1",
+                getBaseUri(), hearingDate, hearingDate);
+        final RequestParams rangeSearchParams = requestParams(rangeSearchUrl, "application/vnd.listing.search.hearings+json")
+                .withHeader(CPP_UID_HEADER.getName(), CPP_UID_HEADER.getValue())
+                .build();
+
+        pollWithDefaults(rangeSearchParams).until(status().is(OK), payload().isJson(allOf(
+                withJsonPath("$.hearings[*].id", hasItem(hearingId)),
+                withJsonPath("$.hearings[0].allocated", is(true)))));
     }
 
     @Test
